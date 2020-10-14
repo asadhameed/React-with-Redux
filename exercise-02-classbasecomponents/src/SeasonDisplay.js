93f5617cae23d3933a9f06a9c4cfdef71afbcb2e
Asad Hameed <asadhameed2002@yahoo.com>
2020-10-14 10:18:55 +0200

    classbase component and also how to use State

---
+++ exercise-02-classbasecomponents/src/SeasonDisplay.js
@@ -0,0 +1,7 @@
+import React from "react-dom";
+
+const SeasonDisplay = () => {
+  return <div>this is season display page</div>;
+};
+
+export default SeasonDisplay;
---
+++ exercise-02-classbasecomponents/src/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null, errorMessage: "" };
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+  render() {
+    if (this.state.lat && !this.state.errorMessage)
+      return <div>User Latitude :{this.state.lat}</div>;
+    if (!this.state.lat && this.state.errorMessage)
+      return <div>Error : {this.state.errorMessage}</div>;
+    return <div>Loading</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
