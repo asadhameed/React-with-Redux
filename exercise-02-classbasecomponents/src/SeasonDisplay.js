9c52711a59e4da970ce1c59584f3957f15c26d9e
Asad Hameed <asadhameed2002@yahoo.com>
2020-10-15 10:21:17 +0200

    create object seasonConfig... remove the duplication of season==='summer'

--- exercise-02-classbasecomponents/src/SeasonDisplay.js
+++ exercise-02-classbasecomponents/src/SeasonDisplay.js
@@ -1,4 +1,15 @@
 import React from "react";
+
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach!",
+    icon: "sun",
+  },
+  winter: {
+    text: "Burr , it's chill!",
+    icon: "snowflake",
+  },
+};
 const getSeason = (lat) => {
   const month = new Date().getMonth();
   return month >= 3 && month <= 8
@@ -12,9 +23,10 @@ const getSeason = (lat) => {
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat);
-  const text =
-    season === "summer" ? "Let's hit the beach!" : "Burr , it's chill!";
-  const icon = season === "summer" ? "sun" : "snowflake";
+  // const text =
+  //   season === "summer" ? "Let's hit the beach!" : "Burr , it's chill!"
+  // const icon = season === "summer" ? "sun" : "snowflake";
+  const { text, icon } = seasonConfig[season];
 
   return (
     <div>
