61864f57ae3fc99f20361f006cd1d3a849324475
Asad Hameed <asadhameed2002@yahoo.com>
2020-10-15 10:14:45 +0200

    add the icon

--- exercise-02-classbasecomponents/src/SeasonDisplay.js
+++ exercise-02-classbasecomponents/src/SeasonDisplay.js
@@ -4,20 +4,23 @@ const getSeason = (lat) => {
   return month >= 3 && month <= 8
     ? lat > 0
       ? "summer"
-      : "Winter"
+      : "winter"
     : lat > 0
     ? "winter"
     : "summer";
 };
 
 const SeasonDisplay = (props) => {
-  return getSeason(props.lat) === "summer" ? (
+  const season = getSeason(props.lat);
+  const text =
+    season === "summer" ? "Let's hit the beach!" : "Burr , it's chill!";
+  const icon = season === "summer" ? "sun" : "snowflake";
+
+  return (
     <div>
-      <h1>Let's hit the beach!</h1>
-    </div>
-  ) : (
-    <div>
-      <h1>Burr , it's chill!</h1>
+      <i className={`${icon} icon`} />
+      <h1>{text}</h1>
+      <i className={`${icon} icon`} />
     </div>
   );
 };
