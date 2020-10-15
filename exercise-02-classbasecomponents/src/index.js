81a290dae2371c9d4e9bb064d7e64000a3a0325e
Asad Hameed <asadhameed2002@yahoo.com>
2020-10-15 13:24:11 +0200

    if there is Multiple return statements in the render better keep in the helper function

--- exercise-02-classbasecomponents/src/index.js
+++ exercise-02-classbasecomponents/src/index.js
@@ -14,12 +14,23 @@ class App extends React.Component {
   componentDidUpdate() {
     console.log("When the componentDidUpdate");
   }
-  render() {
+  /******************
+   * Suppose we want to add boarder around element.style
+   * In this case we will add the following div around all 3 return
+   *  <div className="boarder red">
+   * which is not an good approach
+   *  first we will make some helper function
+   */
+  renderContent = () => {
     if (this.state.lat && !this.state.errorMessage)
       return <SeasonDisplay lat={this.state.lat} />;
     if (!this.state.lat && this.state.errorMessage)
       return <div>Error : {this.state.errorMessage}</div>;
     return <Spinner message="Please accept location request" />;
+  };
+
+  render() {
+    return <div>{this.renderContent()}</div>;
   }
 }
 
