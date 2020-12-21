a5e6c312d4f609ef36ca9a2f5a080b2793ae06e0
Asad Hameed <asadhameed2002@yahoo.com>
2020-12-21 11:01:53 +0100

    Show posts list

--- exercise-06-react-redux-blog/src/component/PostList.js
+++ exercise-06-react-redux-blog/src/component/PostList.js
@@ -5,15 +5,29 @@ import { fetchPosts } from "../actions";
 class PostList extends React.Component {
   componentDidMount() {
     this.props.fetchPosts();
-    //console.log(this.props.fetchPosts());
   }
+
+  renderList() {
+    return this.props.posts.map((post) => {
+      return (
+        <div className="item" key={post.id}>
+          <div className="content">
+            <div className="description">
+              <h1> {post.title}</h1>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  }
+
   render() {
     console.log(this.props.posts);
-    return <div>Post List</div>;
+    return <div className="ui relaxed divided list">{this.renderList()}</div>;
   }
 }
 const mapStateToProp = (state) => {
-  //console.log(state);
   return { posts: state.posts };
 };
 
