661ce09e2e28d62e8f87038f6eadc0ca70fb18b1
Asad Hameed <asadhameed2002@yahoo.com>
2020-12-22 20:36:02 +0100

    One Time Memoization

--- exercise-06-react-redux-blog/src/actions/index.js
+++ exercise-06-react-redux-blog/src/actions/index.js
@@ -1,4 +1,5 @@
 import jsonPlaceHolder from "../api/jsonplaceholder";
+import _ from "lodash";
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceHolder.get("/posts");
   dispatch({
@@ -7,10 +8,14 @@ export const fetchPosts = () => async (dispatch) => {
   });
 };
 
-export const fetchUser = (userId) => async (dispatch) => {
-  const response = await jsonPlaceHolder.get("/users/" + userId);
+export const fetchUser = (userId) => (dispatch) => _fetchUser(userId, dispatch);
+
+// without memoize the function is called many time
+// with memoize the networking call just one time
+const _fetchUser = _.memoize(async (userId, dispatch) => {
+  const response = await jsonPlaceHolder.get(`/users/${userId}`);
   dispatch({
     type: "FETCH_User",
     payLoad: response.data,
   });
-};
+});
