c2f4d1002873c540c685a399f0fe4d68045393ff
asadhameed <asadhameed2002@yahoo.com>
2022-06-30 21:27:46 +0200

    testing with local email

--- exercise-12-basic/src/App.js
+++ exercise-12-basic/src/App.js
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpenses from "./components/NewExpenses/NewExpenses";
-// Add commment from company id
-
 function App() {
   const [expenses, setExpenses] = useState([
     {
