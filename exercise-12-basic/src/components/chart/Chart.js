b91c236e639be7c5a53dafeea56f136cdbd90b54
Asad Hameed Afridi <asadhameed2002@yahoo.com>
2022-06-30 18:53:04 +0200

    add chart

--- exercise-12-basic/src/components/chart/Chart.js
+++ exercise-12-basic/src/components/chart/Chart.js
@@ -1,5 +1,13 @@
 import React from "react";
 import "./Chart.css";
-const Chart = () => {};
+const Chart = (props) => {
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar value={dataPoint.value} maxValue={null} />
+      ))}
+    </div>
+  );
+};
 
 export default Chart;
--- exercise-12-basic/src/components/chart/ChartBar.js
+++ exercise-12-basic/src/components/chart/ChartBar.js
@@ -2,7 +2,14 @@ import React from "react";
 import "./ChartBar.css";
 
 const ChartBar = () => {
-  return <div></div>;
+  return (
+    <div className="chart-bar ">
+      <div className="chart-bar__inne">
+        <div className="chart-bar__fill"></div>
+      </div>
+      <div className="chart-bar__label"></div>
+    </div>
+  );
 };
 
 export default ChartBar;
